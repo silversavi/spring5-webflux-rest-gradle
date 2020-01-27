79606b74031450db9c82149ef6e5017b43838115
Silver Savi <silver.savi@gmail.com>
2020-01-27 23:59:42 +0200

    initial commit

---
+++ src/main/java/ee/slvr/spring5webfluxrestgradle/Spring5WebfluxRestGradleApplication.java
@@ -0,0 +1,13 @@
+package ee.slvr.spring5webfluxrestgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebfluxRestGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebfluxRestGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/slvr/spring5webfluxrestgradle/Spring5WebfluxRestGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.slvr.spring5webfluxrestgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebfluxRestGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
